4316f1ab2398e76e8a62c89e8a84f3f19a77bb4d
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2022-05-07 12:43:51 +0100

    first test with lunar lander

--- fetch-slide.py
+++ fetch-slide.py
@@ -21,18 +21,24 @@ FUNCTIONS DEFINITIONS
 """
 def main():
     env = gym.make('FetchSlide-v1')
-
+    
     for i_episode in range(20):
         observation = env.reset()
-        
+
         for t in range(100):
             env.render()
-            print(observation)
+            
             action = env.action_space.sample()
+            print(env.action_space)
+            print(action)
+
+            # reward scale is environment dependent, but must always increase
             observation, reward, done, info = env.step(action)
+            
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
                 break
+
     env.close()
 
 """
